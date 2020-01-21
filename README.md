dc52501096447d9a0ff1ae61c22ac4a3002b3670
Tyler <sirtytyz@gmail.com>
2020-01-21 12:27:41 -0600

    Got app in working condition
    Finished Generation code
    Updated README

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # evolve
 A start at messing with genetic algorithms.
 
+A creature has 6 traits that can be adjusted to increase or decrease evolutionary success
 
-Inspired to try my hand at creating a genetic algorithm to imitate the existance of a population over time with the chance of having mutations. 
+   * **speed:** Determines how fast a creature moves
+   * **size:** Determines how much food a creature can consume
+   * **maxSize:** Determines the biggest size a creature can achieve through growing
+   * **energy:** How long a creature can go without eating
+   * **safety:** A determining statistic to help a creature decide if it is ready to reproduce
+   * **growthRate:** How much a creature's size increases when it has a surplus of food
+
+
+The environment has 6 traits that can be adjusted to affect all creatures
+
+  * **FOOD_ABUNDANCE:** Percentage chance how often a creature will find food
+  * **ENERGY_CONSUMPTION:** How much energy creatures burn throughout the day
+  * **DAYS_BETWEEN_SEASON:** How many days before a new generation of creatures is born
+  * **GENERATIONS:** How many generations of creatures will be born before the simulation is stopped
+  * **MUTATION_SEVERITY:** How strong will mutations be if they happen to a creature
+  * **MUTATION_CHANCE:** Likelihood of a creature having a mutation 
+
 
 [This article is](https://medium.com/sigmoid/https-medium-com-rishabh-anand-on-the-origin-of-genetic-algorithms-fc927d2e11e0) what helped me get a grasp on where I needed to start. I also got inspiration from watching [Primer](https://www.youtube.com/channel/UCKzJFdi57J53Vr_BkTfN3uQ).
 
--- evolve.py
+++ evolve.py
@@ -1,5 +1,14 @@
 import random
+from os import system, name
 
+
+def clearScreen():
+    if name == 'nt':
+        _= system('cls')
+    else:
+        _= system('clear')
+
+clearScreen()
 # ---Entities---
 class Creature:
     def __init__(self, id):
@@ -11,15 +20,16 @@ class Creature:
     energy = 100
     safety = 100
     growthRate = 10
+    hasMated = False
 class Food:
-    energyValue = 50
+    energyValueRange = [30, 50]
 
-FOOD_ABUNDANCE = 50
-ENERGY_CONSUMPTION = 50
+FOOD_ABUNDANCE = 33
+ENERGY_CONSUMPTION = 40
 DAYS_BETWEEN_SEASON = 1
 GENERATIONS = 10
-MUTATION_SEVERITY = .1
-MUTATION_CHANCE = 10
+MUTATION_SEVERITY = .2
+MUTATION_CHANCE = 20
 
 # ---Fitness---
 livingCreatures = []
@@ -32,7 +42,7 @@ while len(livingCreatures) < 10:
 def searchForFood(hungryBoi):
     if random.randint(0,100) > FOOD_ABUNDANCE:
         if hungryBoi.energy < hungryBoi.size:
-            hungryBoi.energy += Food.energyValue
+            hungryBoi.energy += random.randint(Food.energyValueRange[0], Food.energyValueRange[1])
 
 def cycleDay(livingCreatures):
     survivingCreatures = []
@@ -41,11 +51,12 @@ def cycleDay(livingCreatures):
         if c.energy > 0:
             searchForFood(c)
             survivingCreatures.append(c)
-            print(c.id, c.speed, c.size, c.energy)
+            print("Creature", c.id, "- Speed:", c.speed, "| Size:", c.size, "| Energy:", c.energy, "| Safety:", c.safety)
         else:
-            print("A creature has perished...")
+            print("Creature", c.id ,"has perished...")
         
     return survivingCreatures
+
 
 for i in range(0, DAYS_BETWEEN_SEASON):
     if DAYS_BETWEEN_SEASON > 1:
@@ -62,20 +73,19 @@ def mutate(trait):
         mutation = 0
         mutantLikelihood = random.randint(1,101)
         if mutantLikelihood <= MUTATION_CHANCE:
-            mutation = trait
-            mutation = mutation * MUTATION_SEVERITY
+            mutation = float(trait) * MUTATION_SEVERITY
 
         return trait + mutation
 
-def findMate(male):
+def createOffspring(male):
     for female in livingCreatures:
-        if male.id != female.id and female.energy >= female.safety:
+        if male.id != female.id and female.energy >= female.safety and female.hasMated == False:
             offspring = Creature(livingCreatures[len(livingCreatures)-1].id + 1)
 
             # Speed
             luck = random.randint(0,3)
             if luck == 0:
-                offspring.speed = (male.speed * female.speed) / 2
+                offspring.speed = (male.speed + female.speed) / 2
             elif luck == 1:
                 offspring.speed = male.speed
             elif luck == 2:
@@ -85,7 +95,7 @@ def findMate(male):
             # Size
             luck = random.randint(0,3)
             if luck == 0:
-                offspring.size = (male.size * female.size) / 2
+                offspring.size = (male.size + female.size) / 2
             elif luck == 1:
                 offspring.size = male.size
             elif luck == 2:
@@ -93,9 +103,9 @@ def findMate(male):
             offspring.size = mutate(offspring.size)
 
             # Max Size
-            luck = random.randint(1,101)
+            luck = random.randint(0,3)
             if luck == 0:
-                offspring.maxSize = (male.maxSize * female.maxSize) / 2
+                offspring.maxSize = (male.maxSize + female.maxSize) / 2
             elif luck == 1:
                 offspring.maxSize = male.maxSize
             elif luck == 2:
@@ -105,7 +115,7 @@ def findMate(male):
             # Safety
             luck = random.randint(0,3)
             if luck == 0:
-                offspring.safety = (male.safety * female.safety) / 2
+                offspring.safety = (male.safety + female.safety) / 2
             elif luck == 1:
                 offspring.safety = male.safety
             elif luck == 2:
@@ -115,23 +125,41 @@ def findMate(male):
             # Growth Rate
             luck = random.randint(0,3)
             if luck == 0:
-                offspring.growthRate = (male.growthRate * female.growthRate) / 2
+                offspring.growthRate = (male.growthRate + female.growthRate) / 2
             elif luck == 1:
                 offspring.growthRate = male.growthRate
             elif luck == 2:
                 offspring.growthRate = female.growthRate
             offspring.growthRate = mutate(offspring.growthRate)
 
-            livingCreatures.append(offspring)
+            
+            female.hasMated = True
+            male.hasMated = True
+            offspring.hasMated = True
+            return offspring
+        
+        
 
-for c in livingCreatures:
-    if c.energy >= c.safety and c.size < c.maxSize:
-        c.size += c.growthRate
+for i in range(0, GENERATIONS - 1):
+    if GENERATIONS > 1:
+        print("==== GENERATION ", i + 2, "====*")
 
-    if c.energy >= c.safety:
-        findMate(c)
+    for c in livingCreatures:
+        if c.energy >= c.safety and c.size < c.maxSize:
+            c.size += c.growthRate
 
-for c in livingCreatures:
-    print(c.id, c.speed, c.size, c.energy)
+        if c.energy >= c.safety and c.hasMated == False:
+            baby = createOffspring(c)
+            if baby:
+                livingCreatures.append(baby)
 
+    for c in livingCreatures:
+        c.hasMated = False
+
+    if GENERATIONS > 1:
+        livingCreatures = cycleDay(livingCreatures)
+
+        if len(livingCreatures) <= 0:
+            print("☠ ALL CREATURES HAVE PERISHED ☠")
+            break
 # ---Mutation---
